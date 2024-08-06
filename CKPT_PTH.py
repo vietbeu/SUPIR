96b3724d954d0c6d15c152edf26c52f94be1a77f
vietbeu <vietbeu1998@gmail.com>
2024-08-06 03:03:08 +0000

    update ckpt path

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,4 @@
-LLAVA_CLIP_PATH = '/opt/data/private/AIGC_pretrain/LLaVA1.5/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = '/opt/data/private/AIGC_pretrain/LLaVA1.5/llava-v1.5-13b'
-SDXL_CLIP1_PATH = '/opt/data/private/AIGC_pretrain/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = '/opt/data/private/AIGC_pretrain/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
+LLAVA_CLIP_PATH = 'checkpoints/SUPIR/LLaVA1.5/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = 'checkpoints/SUPIR/LLaVA1.5/llava-v1.5-13b'
+SDXL_CLIP1_PATH = 'checkpoints/SUPIR/clip-vit-large-patch14'
+SDXL_CLIP2_CKPT_PTH = 'checkpoints/SUPIR/CLIP-ViT-bigG-14-laion2B-39B-b160k/open_clip_pytorch_model.bin'
